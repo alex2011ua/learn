076743192ff7abc162c778341e9fa47ab0a29f97
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-02-17 11:25:53 +0200

    1

--- re_test.py
+++ re_test.py
@@ -1,5 +1,7 @@
-summ = 0
-n = int(input())
-for i in range(1, n+1):
-    summ += (1/i**2)
-print('{0:.6f}'.format(summ))
+import math
+a = float(input())
+b = a - int(a)
+if b == 0.5:
+    print(math.ceil(a))
+else:
+    print(round(a))
