131231694a98be20f77e180f53481a297b1c9438
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-02-18 16:43:01 +0200

    создание файла при отсутствии базы

--- re_test.py
+++ re_test.py
@@ -1,9 +1,2 @@
-a = float(input())
-b = float(input())
-c = float(input())
-d = float(input())
-e = float(input())
-f = float(input())
-y = (a*f - e*c)/(a*d - b*c)
-x = (e*d - b*f)/(a*d - b*c)
-print(x, y)
+a = [1,2,3]
+z,x,c = a
