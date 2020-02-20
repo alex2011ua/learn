d587d31db3248f28616028881ae6af35b297e27c
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-02-20 17:42:03 +0200

    3

--- re_test.py
+++ re_test.py
@@ -1,2 +1,21 @@
-a = [1,2,3]
-z,x,c = a
+def poisk(list2):
+    list2.sort()
+    summ = 0
+    i = 0
+    while summ < inp[0]:
+        summ += list2[i]
+        i += 1
+    return i - 1
+
+
+inp = list(map(int, (input().split())))
+list_1 = []
+summ = 0
+for i in range(inp[1]):
+    s = int(input())
+    list_1.append(s)
+    summ += s
+if summ <= inp[0]:
+    print(inp[1])
+else:
+    print(poisk(list_1))
