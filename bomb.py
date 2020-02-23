c180f747017baba95da9c52f2465a03fbeaa9083
AlexUA <60536573+alex2011ua@users.noreply.github.com>
2020-02-23 21:59:40 +0200

    задание про бомбоубежище2

--- bomb.py
+++ bomb.py
@@ -1,9 +1,9 @@
 n = int(input())
 list_city = []
 for i in range(n):
-    list_city.append(tuple(int(input()), i))
+    list_city.append(tuple(map(int, input().split()), i))
 m = int(input())
 list_bomb = []
 for i in range(m):
-    list_bomb.append(tuple(int(input()), i))
+    list_bomb.append(tuple(map(int, input().split()), i))
 list_city.sort()
