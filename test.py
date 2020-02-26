5c458520c93ccd8350d024023320e2f6c3a31f4f
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-02-26 17:44:22 +0200

    1

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-import sys
+n = int(input())
+my_set = set(i for i in range(1, n+1))
+line_set = input()
+while line_set != 'HELP':
 
-for line in sys.stdin:
-    line = line.rstrip()
-    # process line
+    line_set = set(map(int, line_set.split()))
 
+    my_set_yes = my_set & line_set
 
+    if len(my_set_yes)*2 > len(my_set):
+        print("YES")
+        my_set = my_set_yes
+
+    else:
+        print('NO')
+        my_set -= line_set
+
+    line_set = input()
+my_list = sorted(list(my_set))
+print(*my_list)
