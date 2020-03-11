53b145873ebdf7e5d1d47dd000ff388b8d47175e
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-03-11 12:02:12 +0200

    матрица

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
-my_list = []
-my_dict = {}
-count_line = 0
-
-def mu_print(*arg):
-    with open("output.txt", 'w', encoding='utf8') as file:
-        for item in arg:
-            lim = str(arg + '\n')
-            file.writelines(lim)
+from sys import stdin
+import copy
 
 
-with open('input.txt', encoding='utf8') as file:
-    for line in file:
-        count_line += 1
-        s = line.strip()
-        my_dict[s] = my_dict.get(s, 0) + 1
-for key, count in my_dict.items():
-    my_list.append((-count, key))
-my_list.sort()
-print(my_list[0][0]*-2, '&', count_line)
-if my_list[0][0] * -2 > count_line:
-    mu_print(my_list[0][1])
-else:
-    mu_print(my_list[0][1], my_list[1][1])
+class Matrix():
+    def __init__(self, mylst):
+        self.list = copy.deepcopy(mylst)
+        self.col_stroka = len(self.list)
+        self.col_stolbec = len(self.list[0])
+
+    def __str__(self):
+        my_str = ""
+        for line in self.list:
+            for column in line:
+                my_str = my_str + str(column) + "\t"
+            my_str = my_str.strip() + "\n"
+        return my_str.strip()
+
+    def size(self):
+        return self.col_stroka, self.col_stolbec
+
+
+exec(stdin.read())
