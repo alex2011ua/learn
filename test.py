af3d2371ddb4358075c09d270a50cbdf1b45a64f
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-03-11 15:34:06 +0200

    матрица

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from sys import stdin
 import copy
 
+
 class Matrix():
     def __init__(self, mylst):
         self.list = copy.deepcopy(mylst)
         self.col_stroka = len(self.list)
-        self.col_stolbec = len(self.list[0])
-
+        self.col_stolb = len(self.list[0])
 
     def __str__(self):
         my_str = ""
@@ -17,7 +17,25 @@ class Matrix():
         return my_str.strip()
 
     def size(self):
-        return self.col_stroka, self.col_stolbec
+        return self.col_stroka, self.col_stolb
+
+    def __add__(self, other):
+        result_matrix = [[0] * self.col_stolb for i in range(self.col_stroka)]
+        for line in range(self.col_stroka):
+            for column in range(self.col_stolb):
+                item = self.list[line][column] + other.list[line][column]
+                result_matrix[line][column] = item
+        return Matrix(result_matrix)
+
+    def __mul__(self, cislo):
+        result_matrix = [[0] * self.col_stolb for i in range(self.col_stroka)]
+        for line in range(self.col_stroka):
+            for column in range(self.col_stolb):
+                item = self.list[line][column] * cislo
+                result_matrix[line][column] = item
+        return Matrix(result_matrix)
+
+    __rmul__ = __mul__
 
 
 exec(stdin.read())
