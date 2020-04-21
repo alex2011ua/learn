47f26bee6c9f89c0e0cc6380fde6b2a49962c336
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-04-21 12:01:05 +0300

    квадратное уравнение

--- solution.py
+++ solution.py
@@ -1,8 +1,21 @@
 import sys
-
+import math
 
 try:
-    digit_string = int(sys.argv[1])
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    c = int(sys.argv[3])
 except:
-    digit_string = 5
-print(digit_string)
+    a = 1
+    b = -3
+    c = -4
+
+D = b * b - 4 * a * c
+if D > 0:
+    x1 = (-b + math.sqrt(D)) / 2 * a
+    x2 = (-b - math.sqrt(D)) / 2 * a
+    print(x1, x2, sep = "\n")
+
+if D == 0:
+    x = - b/2 * a
+    print(x)
