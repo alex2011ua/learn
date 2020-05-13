4050fac63ea86d9bfbb5a911dd54c80220194355
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-05-13 11:31:28 +0300

    oop

--- oop/base.py
+++ oop/base.py
@@ -3,12 +3,6 @@ from abc import ABC, abstractmethod
 
 
 class Base(ABC):
-
-
-    pass
-
-
-class A:
     def __init__(self, data, result):
         self.data = data
         self.result = result
@@ -16,6 +10,16 @@ class A:
     def get_answer(self):
         return [int(x >= 0.5) for x in self.data]
 
+    @abstractmethod
+    def get_score(self):
+        pass
+
+    @abstractmethod
+    def get_loss(self):
+        pass
+
+
+class A(Base):
     def get_score(self):
         ans = self.get_answer()
         return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
@@ -26,14 +30,7 @@ class A:
             [(x - y) * (x - y) for (x, y) in zip(self.data, self.result)])
 
 
-class B:
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
-    def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
-
+class B(Base):
     def get_loss(self):
         return -sum([
             y * math.log(x) + (1 - y) * math.log(1 - x)
@@ -56,14 +53,7 @@ class B:
         return 2 * pre * rec / (pre + rec)
 
 
-class C:
-    def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
-    def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
-
+class C(Base):
     def get_score(self):
         ans = self.get_answer()
         return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
