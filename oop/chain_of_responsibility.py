44f58cfa874afed8682f4823a3ab0cd400c729bc
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-05-26 17:31:43 +0300

    chain_of_responsibility.

--- oop/chain_of_responsibility.py
+++ oop/chain_of_responsibility.py
@@ -5,18 +5,58 @@ class SomeObject:
         self.string_field = ""
 
 
+class EventGet:
+    def __init__(self, kind):
+        self.get = kind
+        self.set = None
+
+
+class EventSet:
+    def __init__(self, kind):
+        self.set = kind
+        self.get = None
+
+
 class NullHandler:
-    pass
+    def __init__(self, successor=None):
+        self.__successor = successor
+
+    def handle(self, obj, event):
+        if self.__successor is not None:
+            self.__successor.handle(obj, event)
 
 
 class IntHandler(NullHandler):
-    pass
+    def handle(self, obg, event):
+        if event.get == int:
+            print(obg.integer_field)
+        elif type(event.set) == int:
+            obg.integer_field = event.set
+
+        else:
+
+            super().handle(obg, event)
 
 
-class FloatHandle(NullHandler):
-    pass
+class FloatHandler(NullHandler):
+    def handle(self, obg, event):
+        if event.get == float:
+            print(obg.float_field)
+        elif type(event.set) == float:
+            obg.float_field = event.set
+
+        else:
+
+            super().handle(obg, event)
 
 
 class StrHandler(NullHandler):
-    pass
+    def handle(self, obg, event):
+        if event.get == str:
+            print(obg.string_field)
+        elif type(event.set) == str:
+            obg.string_field = event.set
 
+        else:
+
+            super().handle(obg, event)
