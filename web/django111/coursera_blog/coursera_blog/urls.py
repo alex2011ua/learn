93bbabcfcaf69281bc2f4237e3ce74347dd19cd0
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-07-01 16:15:25 +0300

    work

--- web/django111/coursera_blog/coursera_blog/urls.py
+++ web/django111/coursera_blog/coursera_blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from corre.views import index
+
 
 urlpatterns = [
+    url(r'^index/$', index),
     url(r'^admin/', admin.site.urls),
 ]
