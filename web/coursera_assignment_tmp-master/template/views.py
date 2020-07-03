a28ba74cdb4fe61669b2ec091289c2ab4347bb83
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-07-03 17:01:01 +0300

    django2

--- web/coursera_assignment_tmp-master/template/views.py
+++ web/coursera_assignment_tmp-master/template/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 def echo(request):
-    pass
+    dat = str(datetime.datetime.now())[10:-7]
+    b = request.META
+    a = {}
+    a['statement'] = b.get('HTTP_X-PRINT_STATEMENT')
+
+    a['metod'] = request.method
+    param = None
+    if a['metod'] == 'GET':
+        param = request.GET
+    elif a['metod'] == 'POST':
+        param = request.POST
+
+    if len(param) >= 1:
+        a['key'] = True
+        a['param'] =[]
+        for item, key in param.items():
+            c = str(item)+': '+str(key)
+            a['param'].append(c)
+    else:
+        a['key'] = None
+    print(a[])
+    lis = ['1','2','3']
+    return render(request, 'echo.html', a)
 
 
 def filters(request):
